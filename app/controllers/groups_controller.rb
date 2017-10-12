320a4aac3a770c3701cb9f06d03a437e33853281
Adam Shafe <adam.shafe@onthebeach.co.uk>
2017-10-12 10:00:38 +0100

    Added My Groups sedtion and the Devise views files

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
 def index
-  @groups = Group.all
+  @groups = current_user.groups
 end
 
 def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :groups
 
   get 'groups/new', to: 'groups#new'
   post 'groups/new', to: 'groups#create'
