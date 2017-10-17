19319292be42254e8f24ba792279806e6756bebc
Oliver Butler Jones <oliverbutlerjones@gmail.com>
2017-10-17 09:23:56 +0100

    fixed merge

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20171016103525) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -69,6 +68,9 @@ ActiveRecord::Schema.define(version: 20171016103525) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.datetime "date_of_birth"
+    t.boolean "is_female", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
