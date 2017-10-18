bc67c81c446bf182ce77d1b0edece877fd0d411f
Adam Shafe <adam.shafe@onthebeach.co.uk>
2017-10-18 15:38:43 +0100

    Option to delete suggestions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,12 @@ class CommentsController < ApplicationController
     redirect_back fallback_location: root_path 
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_back fallback_location: root_path
+  end
+
   private
 
   def comment_params
